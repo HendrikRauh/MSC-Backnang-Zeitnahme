e73637474f75ee220566ec702dead8b4586b26ac
Hendrik Rauh <114620133+HendrikRauh@users.noreply.github.com>
2024-10-13 11:35:04 +0200

    MSC-12 added npm install

--- scripts/install/install.bat
+++ scripts/install/install.bat
@@ -9,6 +9,9 @@ start /wait "" "%~dp0PL23XX-M_LogoDriver_Setup_408_20220725.exe"
 echo #Installing Node
 msiexec /i "%~dp0node-v22.9.0-x64.msi" /qb
 
+echo #Installing Packages
+call npm install
+
 echo #Installing PM2
 call npm install -g pm2
 
