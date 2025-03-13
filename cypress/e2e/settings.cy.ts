3f145779cbbccea9f5f6cf3fdd2213dc6867fd3b
Hendrik Rauh <114620133+HendrikRauh@users.noreply.github.com>
2025-03-13 22:35:34 +0100

    feat: enhance settings tests with API operation mode changes and refactor test structure

--- cypress/e2e/settings.cy.ts
+++ cypress/e2e/settings.cy.ts
@@ -1,31 +1,42 @@
-describe("Settings", () => {
-    const openSettings = () => {
+describe("General", () => {
+    it("opens settings", () => {
         cy.visit("/");
         cy.contains("Einstellungen").click();
-    };
-
-    const activateManualMode = () => {
-        openSettings();
-        cy.get("#operationMode").select("Manuell");
-        cy.get("select").should("have.length", 3);
-    };
-
-    it("opens settings", () => {
-        openSettings();
     });
 
     it("activate manual mode", () => {
-        activateManualMode();
+        cy.visit("/settings");
+
+        cy.get("#operationMode").select("Manuell");
+        cy.get("select:visible").should("have.length", 3);
     });
 
     it("activate standalone mode", () => {
-        openSettings();
+        cy.visit("/settings");
+
         cy.get("#operationMode").select("Standalone");
         cy.get("select").should("have.length", 1);
     });
 
+    it("activate manual mode API", () => {
+        cy.request("POST", "/set-operation-mode", { operationMode: "manual" });
+        cy.visit("/settings");
+        cy.get("select:visible").should("have.length", 3);
+    });
+
+    it("activate standalone mode API", () => {
+        cy.request("POST", "/set-operation-mode", {
+            operationMode: "standalone",
+        });
+        cy.visit("/settings");
+        cy.get("select:visible").should("have.length", 1);
+    });
+});
+
+describe("Manual Mode", () => {
     it("manages drivers", () => {
-        activateManualMode();
+        cy.request("POST", "/set-operation-mode", { operationMode: "manual" });
+        cy.visit("/settings");
 
         cy.get("#trainingGroups input[type='checkbox']").each(($el) => {
             cy.wrap($el).uncheck();
@@ -40,7 +51,6 @@ describe("Settings", () => {
         });
 
         //TODO: select drivers from inactive and click activate. amount of selected should move to active
-
         cy.contains("RESET").click();
         cy.get("#activeDrivers").find("option").should("have.length", 0);
     });
