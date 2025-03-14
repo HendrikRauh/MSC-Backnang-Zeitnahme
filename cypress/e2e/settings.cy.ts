2981a7f6ec8818d0a345b36834563c40f74ac296
Hendrik Rauh <114620133+HendrikRauh@users.noreply.github.com>
2025-03-14 22:35:20 +0100

    splited driver management up

--- cypress/e2e/settings.cy.ts
+++ cypress/e2e/settings.cy.ts
@@ -28,11 +28,13 @@ describe("General", () => {
     });
 });
 
-describe("Manual Mode", () => {
-    it("manages drivers", () => {
+describe("Driver Management Tests", () => {
+    beforeEach(() => {
         cy.request("POST", "/set-operation-mode", { operationMode: "manual" });
         cy.visit("/settings");
+    });
 
+    it("filter trainingGroups", () => {
         cy.get("#trainingGroups input[type='checkbox']").each(($el) => {
             cy.wrap($el).uncheck();
         });
@@ -40,81 +42,102 @@ describe("Manual Mode", () => {
         cy.get("#inactiveDrivers")
             .find("option:visible")
             .should("have.length", 0);
-
-        cy.get("#trainingGroups input[type='checkbox']").each(($el) => {
-            cy.wrap($el).check();
-        });
-
-        cy.get("#activeDrivers")
-            .find("option")
-            .then(($activeDrivers) => {
-                const activeDriversBefore = $activeDrivers.length;
-
-                cy.get("#inactiveDrivers").select(0);
-                cy.contains("AKTIVIEREN").click();
-                cy.get("#activeDrivers")
-                    .find("option")
-                    .should("have.length.greaterThan", activeDriversBefore);
-
-                cy.get("#inactiveDrivers")
-                    .find("option")
-                    .its("length")
-                    .should(
-                        "be.lessThan",
-                        Cypress.$("#inactiveDrivers option").length
-                    );
-            });
-
-        cy.get("#activeDrivers")
-            .find("option")
-            .then(($activeDrivers) => {
-                const activeDriversBefore = $activeDrivers.length;
-
-                cy.get("#activeDrivers").select(0);
-                cy.contains("DEAKTIVIEREN").click();
-                cy.get("#activeDrivers")
-                    .find("option")
-                    .should("have.length.lessThan", activeDriversBefore);
-
-                cy.get("#inactiveDrivers")
-                    .find("option")
-                    .its("length")
-                    .should(
-                        "be.above",
-                        Cypress.$("#inactiveDrivers option").length
-                    );
-            });
-
-        cy.get("#activeDrivers")
-            .find("option")
-            .then(($options) => {
-                const totalOptions = $options.length;
-
-                cy.get("#activeDrivers").select(1);
-                cy.contains("HOCH").click();
-
-                cy.get("#activeDrivers")
-                    .find("option:first")
-                    .invoke("text")
-                    .should("eq", Cypress.$($options[1]).text());
-            });
-
-        cy.get("#activeDrivers")
-            .find("option")
-            .then(($options) => {
-                const totalOptions = $options.length;
-
-                cy.get("#activeDrivers").select(0);
-                cy.contains("RUNTER").click();
-
-                cy.get("#activeDrivers")
-                    .find("option:nth-child(2)")
-                    .invoke("text")
-                    .should("eq", Cypress.$($options[0]).text());
-            });
-        cy.contains("RESET").click();
-        cy.get("#activeDrivers").find("option").should("have.length", 0);
     });
 
-    it("deletes all times");
+    describe("Driver Activation", () => {
+        beforeEach(() => {
+            cy.get("#trainingGroups input[type='checkbox']").each(($el) => {
+                cy.wrap($el).check();
+            });
+        });
+
+        it("should activate drivers correctly", () => {
+            cy.get("#activeDrivers")
+                .find("option")
+                .then(($activeDrivers) => {
+                    const activeDriversBefore = $activeDrivers.length;
+
+                    cy.get("#inactiveDrivers").select(0);
+                    cy.contains("AKTIVIEREN").click();
+
+                    cy.get("#activeDrivers")
+                        .find("option")
+                        .should("have.length.greaterThan", activeDriversBefore);
+
+                    cy.get("#inactiveDrivers")
+                        .find("option")
+                        .its("length")
+                        .should(
+                            "be.lessThan",
+                            Cypress.$("#inactiveDrivers option").length
+                        );
+                });
+        });
+    });
+
+    describe("Driver Deactivation", () => {
+        it("should deactivate drivers correctly", () => {
+            cy.get("#activeDrivers")
+                .find("option")
+                .then(($activeDrivers) => {
+                    const activeDriversBefore = $activeDrivers.length;
+
+                    cy.get("#activeDrivers").select(0);
+                    cy.contains("DEAKTIVIEREN").click();
+
+                    cy.get("#activeDrivers")
+                        .find("option")
+                        .should("have.length.lessThan", activeDriversBefore);
+
+                    cy.get("#inactiveDrivers")
+                        .find("option")
+                        .its("length")
+                        .should(
+                            "be.above",
+                            Cypress.$("#inactiveDrivers option").length
+                        );
+                });
+        });
+    });
+
+    describe("Driver Sorting", () => {
+        it("should move driver up correctly", () => {
+            cy.get("#activeDrivers")
+                .find("option")
+                .then(($options) => {
+                    const totalOptions = $options.length;
+
+                    cy.get("#activeDrivers").select(1);
+                    cy.contains("HOCH").click();
+
+                    cy.get("#activeDrivers")
+                        .find("option:first")
+                        .invoke("text")
+                        .should("eq", Cypress.$($options[1]).text());
+                });
+        });
+
+        it("should move driver down correctly", () => {
+            cy.get("#activeDrivers")
+                .find("option")
+                .then(($options) => {
+                    const totalOptions = $options.length;
+
+                    cy.get("#activeDrivers").select(0);
+                    cy.contains("RUNTER").click();
+
+                    cy.get("#activeDrivers")
+                        .find("option:nth-child(2)")
+                        .invoke("text")
+                        .should("eq", Cypress.$($options[0]).text());
+                });
+        });
+    });
+
+    describe("Reset Functionality", () => {
+        it("should reset driver management state", () => {
+            cy.contains("RESET").click();
+            cy.get("#activeDrivers").find("option").should("have.length", 0);
+        });
+    });
 });
